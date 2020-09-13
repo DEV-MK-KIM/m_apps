26ce24b36f85b87ef37b3ccf43ae47ea4b304c0e
MK Kim <mk.kim.yyc@gmail.com>
2020-09-12 22:10:51 -0600

    added more widgetes

--- expenseTracker/lib/main.dart
+++ expenseTracker/lib/main.dart
@@ -1,7 +1,7 @@
+import 'package:expenseTracker/widgets/user_transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
-import './transaction.dart';
+import './widgets/user_transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,25 +19,8 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.19,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Groceries',
-      amount: 216.11,
-      date: DateTime.now(),
-    )
-  ];
-
   // String titleInput;
   // String amountInput;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -56,86 +39,7 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Card(
-              elevation: 4,
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    TextField(
-                        decoration: InputDecoration(labelText: "Title"),
-                        controller: titleController
-
-                        // onChanged: (val) {
-                        //   titleInput = val;
-                        // },
-                        ),
-                    TextField(
-                        decoration: InputDecoration(labelText: "Amount"),
-                        controller: amountController
-                        // onChanged: (val) {
-                        //   amountInput = val;
-                        // },
-                        ),
-                    FlatButton(
-                      child: Text("Add Transaction"),
-                      textColor: Colors.deepPurple,
-                      onPressed: () {
-                        print(titleController.text);
-                        print(amountController.text);
-                      },
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Column(
-              children: transactions
-                  .map((tx) => Card(
-                          child: Row(
-                        children: <Widget>[
-                          Container(
-                            margin: EdgeInsets.symmetric(
-                              vertical: 10,
-                              horizontal: 15,
-                            ),
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.purple,
-                                width: 2,
-                              ),
-                            ),
-                            padding: EdgeInsets.all(10),
-                            child: Text(
-                              "\$${tx.amount}",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 15,
-                                color: Colors.purple,
-                              ),
-                            ),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                tx.title,
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 18),
-                              ),
-                              Text(
-                                DateFormat.yMMMd().format(tx.date),
-                                style: TextStyle(
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            ],
-                          )
-                        ],
-                      )))
-                  .toList(),
-            )
+            UserTransactions(),
           ],
         ));
   }
