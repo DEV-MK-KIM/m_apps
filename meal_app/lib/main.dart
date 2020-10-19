3d021f6f2a91f49dc9ec3e774ed6dcd53e08b4d1
MK Kim <mk.kim.yyc@gmail.com>
2020-10-18 23:27:06 -0600

    changed from stateless to stateful in maon

--- meal_app/lib/main.dart
+++ meal_app/lib/main.dart
@@ -1,14 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/dummy_data.dart';
 
 import './screen/category_meals_screen.dart';
 import './screen/categories_screen.dart';
 import './screen/meal_detail_screen.dart';
 import './screen/tabs_screen.dart';
 import './screen/filters_screen.dart';
+import './models/meal.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    'gluten': false,
+    'lactose': false,
+    'vegan': false,
+    'vegetarian': false,
+  };
+
+  List<Meal> _availableMeals = DUMMY_MEALS;
+
+  void _setFilters(Map<String, bool> filterData) {
+    setState(() {
+      print(_filters);
+
+      _filters = filterData;
+      print(_filters);
+
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        if (_filters['gluten'] && !meal.isGlutenFree) {
+          return false;
+        }
+        if (_filters['lactose'] && !meal.isLactoseFree) {
+          return false;
+        }
+        if (_filters['vegan'] && !meal.isVegan) {
+          return false;
+        }
+        if (_filters['vegetarian'] && !meal.isVegetarian) {
+          return false;
+        }
+        return true;
+      }).toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -37,9 +78,10 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (ctx) => TabsScreen(), // home can be replaced
         // '/category-meals': (ctx) => CategoryMealsScreen(), // can be replaced as below to prevent type error
-        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        CategoryMealsScreen.routeName: (ctx) =>
+            CategoryMealsScreen(_availableMeals),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
-        FiltersScreen.routeName: (ctx) => FiltersScreen(),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(_filters, _setFilters),
       },
       // any non-registered route will be redirected as below
       onGenerateRoute: (settings) {
